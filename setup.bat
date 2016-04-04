f95293afbbe1507f5f896455e35908b5df6605c3
dcharb <dcharb@users.noreply.github.com>
2016-04-04 15:10:15 -0400

    Update setup to include other vimfiles directory

--- setup.bat
+++ setup.bat
@@ -1,2 +1,3 @@
 git clone https://github.com/VundleVim/Vundle.vim.git %USERPROFILE%\vimfiles\bundle\Vundle.vim
-
+echo set rtp+=$HOME/vimfiles-other > %USERPROFILE%\_vimrc
+echo source $HOME/vimfiles/vimrc >> %USERPROFILE%\_vimrc
